f38c01a37b0bee36d382087b1310ebb4d7cb6c86
Neil Jackson <neil@rattnow.com>
2020-08-03 12:54:41 -0700

    printed a drill holder -- far too weak

--- miraclegrue_config_transform.py
+++ miraclegrue_config_transform.py
@@ -9,26 +9,56 @@ def transformMiraclegrueConfig(config: dict):
     config['layerHeight'] = 0.4
     config['baseLayerHeight'] = 0.4
     config['baseLayerWidth'] = 0.9
-    config['bedZOffset'] = -0.25
+    config['bedZOffset'] = -0.37
     config['doFixedShellStart'] = False
-    config['extruderProfiles'][0]['extrusionProfiles']['base_layer_surface']['extrusionVolumeMultiplier'] = 1.85
+    config['extruderProfiles'][0]['extrusionProfiles']['base_layer_surface']['extrusionVolumeMultiplier'] = 1.35
     config['extruderProfiles'][0]['extrusionProfiles']['base_layer_surface']['fanSpeed'] = 0
     config['extruderProfiles'][0]['extrusionProfiles']['base_layer_surface']['feedrate'] = 4
+    
+    # fullSpeedFeedrate = 90
+    fullSpeedFeedrate = 70
+    
+    # config['extruderProfiles'][0]['extrusionProfiles']['bridges']                 ['feedrate'] = 40.0
+    # config['extruderProfiles'][0]['extrusionProfiles']['brims']                   ['feedrate'] = 10.0
+    config['extruderProfiles'][0]['extrusionProfiles']['floor_surface']           ['feedrate'] = fullSpeedFeedrate
+    # config['extruderProfiles'][0]['extrusionProfiles']['halfVolumeInfill']        ['feedrate'] = 110.0
+    config['extruderProfiles'][0]['extrusionProfiles']['insets']                  ['feedrate'] = fullSpeedFeedrate
+    # config['extruderProfiles'][0]['extrusionProfiles']['low_speed_shells']        ['feedrate'] = 30
+    # config['extruderProfiles'][0]['extrusionProfiles']['minfillSparse']           ['feedrate'] = 110.0
+    # config['extruderProfiles'][0]['extrusionProfiles']['outlines']                ['feedrate'] = 20
+    # config['extruderProfiles'][0]['extrusionProfiles']['purge']                   ['feedrate'] = 100
+    # config['extruderProfiles'][0]['extrusionProfiles']['raft_base']               ['feedrate'] = 10.0
+    # config['extruderProfiles'][0]['extrusionProfiles']['raft_interface']          ['feedrate'] = fullSpeedFeedrate
+    # config['extruderProfiles'][0]['extrusionProfiles']['raft_surface']            ['feedrate'] = fullSpeedFeedrate
+    config['extruderProfiles'][0]['extrusionProfiles']['roof_surface']            ['feedrate'] = fullSpeedFeedrate
+    config['extruderProfiles'][0]['extrusionProfiles']['solid']                   ['feedrate'] = fullSpeedFeedrate
+    config['extruderProfiles'][0]['extrusionProfiles']['sparse']                  ['feedrate'] = fullSpeedFeedrate
+    config['extruderProfiles'][0]['extrusionProfiles']['sparseRoofSurfaceFills']  ['feedrate'] = fullSpeedFeedrate
+    # config['extruderProfiles'][0]['extrusionProfiles']['spurs']                   ['feedrate'] = 20
+    # config['extruderProfiles'][0]['extrusionProfiles']['support_floor_surface']   ['feedrate'] = 10.0
+    config['extruderProfiles'][0]['extrusionProfiles']['support_roof_surface']    ['feedrate'] = fullSpeedFeedrate
+    config['extruderProfiles'][0]['extrusionProfiles']['support_solid']           ['feedrate'] = fullSpeedFeedrate
+    config['extruderProfiles'][0]['extrusionProfiles']['support_sparse']          ['feedrate'] = fullSpeedFeedrate
+
+    
+    
     config['fanLayer'] = 2
     config['modelFillProfiles']['base_layer_surface']['extrusionWidth'] = 1.5
     config['modelFillProfiles']['sparse']['density'] = 0.1
     config['modelShellProfiles']['base_layer_surface']['infillShellSpacingMultiplier'] = 0.1
     config['modelShellProfiles']['base_layer_surface']['innerExtrusionProfile'] = "base_layer_surface"
+    config['modelShellProfiles']['base_layer_surface']['outerExtrusionProfile'] = "base_layer_surface"
     config['modelShellProfiles']['base_layer_surface']['insetDistanceMultiplier'] = 0.9
     config['modelShellProfiles']['base_layer_surface']['numberOfShells'] = 2
-    config['modelShellProfiles']['base_layer_surface']['outerExtrusionProfile'] = "base_layer_surface"
     config['paddedBaseWidth'] = 2
     config['raftModelShellsSpacing'] = 0.19
     config['doPaddedBase'] = False
     
+    
+    
 
     config['doRaft'] = False
-    config['doSupport'] = True
+    config['doSupport'] = False
     config['doSupportUnderBridges'] = True
     config['doBreakawaySupport'] = True
     
